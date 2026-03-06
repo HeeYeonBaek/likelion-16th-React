630e95535e441cff0b40c55be18d0177c3d49aeb
heeyeon <asd95121@gmail.com>
2026-03-06 13:06:36 +0900

    리액트 학습자료

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+/**
+ * JSX (JavaScript eXtension: 자바스크립트 확장 (비표준: 브라우저 해석 못함: SyntaxError))
+ * 빌드(컴파일 + 번들링) 도구에서만 JSX 사용 가능
+ */
+
+function App() {
+  return null
+}
+
+export default App
