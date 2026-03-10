1cf62d9ffe8d2d451b45836986b79ac20f27b408
heeyeon <asd95121@gmail.com>
2026-03-10 11:14:25 +0900

    업데이트

--- 02-State-이벤트-핸들링/src/learns/UpdateState/index.tsx
+++ 02-State-이벤트-핸들링/src/learns/UpdateState/index.tsx
@@ -1,5 +1,8 @@
+import BatchCounter from './BatchCounter'
 import S from './style.module.css'
 
 export default function UpdateState() {
-  return <div className={S.container}></div>
+  return <div className={S.container}>
+    <BatchCounter />
+  </div>
 }
